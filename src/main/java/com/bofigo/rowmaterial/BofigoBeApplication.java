12cdc702e53b5b979b7026ca09935a5ea859330b
eylmz <emre06ylmz@gmail.com>
2021-09-03 22:09:45 +0300

    initial

--- src/main/java/com/bofigo/rowmaterial/BofigoBeApplication.java
+++ src/main/java/com/bofigo/rowmaterial/BofigoBeApplication.java
@@ -3,6 +3,8 @@ package com.bofigo.rowmaterial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -11,5 +13,6 @@ public class BofigoBeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BofigoBeApplication.class, args);
 	}
+
 
 }
---
+++ src/main/resources/precache-manifest.b901a826d337fa670d51fb6cd47542ea.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "db10a10dc0288f66d7003641bebbad91",
+    "url": "/static/media/vibe-logo.db10a10d.svg"
+  },
+  {
+    "revision": "1ad33b0fdf61933f9629",
+    "url": "/static/css/main.f0085277.chunk.css"
+  },
+  {
+    "revision": "8db0524040f5f8b3834c0eefe65aaca9",
+    "url": "/static/media/avatar2.8db05240.jpeg"
+  },
+  {
+    "revision": "7b5f217291b8f2768855",
+    "url": "/static/js/1.7b5f2172.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71fa438ecc49974c1af4a35aa3626fa9",
+    "url": "/static/media/spin.71fa438e.svg"
+  },
+  {
+    "revision": "febae3c6a394c8a50fd5f4a788b28af4",
+    "url": "/static/media/bars.febae3c6.svg"
+  },
+  {
+    "revision": "b61fecc3e00410ddbaa02a2fde8718ae",
+    "url": "/static/media/puff.b61fecc3.svg"
+  },
+  {
+    "revision": "3fd8845e8372f00349f61b6b1c24007d",
+    "url": "/static/media/dots.3fd8845e.svg"
+  },
+  {
+    "revision": "1ad33b0fdf61933f9629",
+    "url": "/static/js/main.1ad33b0f.chunk.js"
+  },
+  {
+    "revision": "a2d15caa900c55c634969d14c2bdbe78",
+    "url": "/static/media/avatar1.a2d15caa.png"
+  },
+  {
+    "revision": "08484fa4ba6308a7a5d3d19122ac5c56",
+    "url": "/static/media/react-feature.08484fa4.svg"
+  },
+  {
+    "revision": "4d7802134fbf299ad828bf130bd46695",
+    "url": "/static/media/sass-feature.4d780213.svg"
+  },
+  {
+    "revision": "69a7012b0542453d6bc2f43f7766a2c5",
+    "url": "/static/media/bootstrap-feature.69a7012b.svg"
+  },
+  {
+    "revision": "a7b5f5aafbc9c3c2fbec4c339b6e070e",
+    "url": "/static/media/responsive-feature.a7b5f5aa.svg"
+  },
+  {
+    "revision": "a56d13399ca5fce64c22ea391e352cc9",
+    "url": "/static/media/apple.a56d1339.png"
+  },
+  {
+    "revision": "64cc8c4fae9886667bc59b39493c4b2d",
+    "url": "/static/media/microsoft.64cc8c4f.png"
+  },
+  {
+    "revision": "7b5f217291b8f2768855",
+    "url": "/static/css/1.02afd8b2.chunk.css"
+  },
+  {
+    "revision": "4c6c8d95d17dc7c435fabe983c4c9ffb",
+    "url": "/index.html"
+  }
+];
