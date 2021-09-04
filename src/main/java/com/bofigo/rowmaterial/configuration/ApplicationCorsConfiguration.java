7e6cb85eebfe88d6ed162ba436e28655c82f5b7a
eylmz <emre06ylmz@gmail.com>
2021-09-04 16:23:25 +0300

    fix

--- src/main/java/com/bofigo/rowmaterial/configuration/ApplicationCorsConfiguration.java
+++ src/main/java/com/bofigo/rowmaterial/configuration/ApplicationCorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.bofigo.rowmaterial.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -7,6 +8,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class ApplicationCorsConfiguration {
 
+	@Bean
 	public CorsFilter corsFilter() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		RegexCorsConfiguration regexCorsConfiguration = new RegexCorsConfiguration();
